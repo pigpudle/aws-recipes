d932aa37bdb920ec7b52cc9d7aeee57f3a862ad1
Neli Harbuzava <neliharbuzavapublic@gmail.com>
2023-12-18 23:22:40 +0300

    DynamoDB - read item

---
+++ dynamodb/v2/ReadItem.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk');
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const table = 'movies';
+
+const year = 2022;
+const title = 'New Movie';
+
+const params = {
+  TableName: table,
+  Key: {
+    year,
+    title
+  }
+};
+
+docClient.get(params, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+})
