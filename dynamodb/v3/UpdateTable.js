a3415398963f45870a2246eed1a05ed29ca77abe
Neli Harbuzava <Neli.Harbuzava@ventionteams.com>
2023-12-24 18:05:01 +0300

    dynamodb v3 - update table

---
+++ dynamodb/v3/UpdateTable.js
@@ -0,0 +1,22 @@
+import { DynamoDBClient, UpdateTableCommand } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient();
+
+const params = {
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5
+    },
+    TableName: 'employees',
+};
+
+const run = async () => {
+    try {
+        const data = await client.send(new UpdateTableCommand(params));
+        console.log("Success", data.TableDescription);
+    } catch (err) {
+        console.log("Error", err);
+    }
+};
+
+run();
